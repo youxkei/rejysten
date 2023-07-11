e12dd714d454a40c7245651e70334d7393231dbd
youxkei <youxkei@gmail.com>
2023-07-11 14:25:45 +0900

    make for poc stories

--- src/components/poc/for.stories.tsx
+++ src/components/poc/for.stories.tsx
@@ -1,15 +1,108 @@
 import type { Meta, StoryObj } from "storybook-solidjs";
 
-import { PrimitiveForTest, PrimitiveIndexTest } from "@/components/poc/for";
+import { produce } from "immer";
+import { For, Index, createSignal } from "solid-js";
 
 export default {
   title: "poc/for",
 } satisfies Meta;
 
-export const For: StoryObj = {
-  render: () => <PrimitiveForTest />,
+export const ForTest: StoryObj = {
+  render: () => {
+    function Element(props: { value: string }) {
+      console.log("Element");
+
+      const value$ = () => {
+        console.log("value$", props.value);
+
+        return props.value;
+      };
+
+      return <div>{value$()}</div>;
+    }
+
+    const [array$, setArray] = createSignal(["1", "2", "3"], {});
+
+    return (
+      <>
+        <For each={array$()}>{(value) => <Element value={value} />}</For>
+        <button onClick={() => setArray((array) => [`${Number(array[0]) - 1}`, ...array].map((x) => `${x}`))}>prepend</button>
+        <button onClick={() => setArray((array) => [...array, `${Number(array[array.length - 1]) + 1}`].map((x) => `${x}`))}>append</button>
+        <button
+          onClick={() =>
+            setArray(
+              produce((array) => {
+                array[1] = `${Number(array[1]) + 1}`;
+              })
+            )
+          }
+        >
+          change
+        </button>
+        <button
+          onClick={() =>
+            setArray(
+              produce((array) => {
+                const tmp = array[1];
+                array[1] = array[2];
+                array[2] = tmp;
+              })
+            )
+          }
+        >
+          exchange
+        </button>
+      </>
+    );
+  },
 };
 
-export const Index: StoryObj = {
-  render: () => <PrimitiveIndexTest />,
+export const IndexTest: StoryObj = {
+  render: () => {
+    function Element(props: { value: string }) {
+      console.log("Element");
+
+      const value$ = () => {
+        console.log("value$", props.value);
+
+        return props.value;
+      };
+
+      return <div>{value$()}</div>;
+    }
+
+    const [array$, setArray] = createSignal(["1", "2", "3"], {});
+
+    return (
+      <>
+        <Index each={array$()}>{(value) => <Element value={value()} />}</Index>
+        <button onClick={() => setArray((array) => [`${Number(array[0]) - 1}`, ...array].map((x) => `${x}`))}>prepend</button>
+        <button onClick={() => setArray((array) => [...array, `${Number(array[array.length - 1]) + 1}`].map((x) => `${x}`))}>append</button>
+        <button
+          onClick={() =>
+            setArray(
+              produce((array) => {
+                array[1] = `${Number(array[1]) + 1}`;
+              })
+            )
+          }
+        >
+          change
+        </button>
+        <button
+          onClick={() =>
+            setArray(
+              produce((array) => {
+                const tmp = array[1];
+                array[1] = array[2];
+                array[2] = tmp;
+              })
+            )
+          }
+        >
+          exchange
+        </button>
+      </>
+    );
+  },
 };
