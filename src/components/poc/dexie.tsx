81b89256f6d0a435436b7204b56eb5c56a0933cc
youxkei <youxkei@gmail.com>
2023-11-11 15:17:55 +0900

    add dexie story

---
+++ src/components/poc/dexie.tsx
@@ -0,0 +1,8 @@
+import type { Meta, StoryObj } from "storybook-solidjs";
+export default {
+  title: "poc/dexie",
+} satisfies Meta;
+
+export const DexieTransactionLiveQuery: StoryObj = {
+  render: () => null,
+};
